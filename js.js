1c5b3c2e9871635c2b8e82c5b6fb4adc00bef109
Janina Sundström <janina.sundstrom@chasacademy.se>
2022-02-01 10:48:23 +0100

    ok

--- js.js
+++ js.js
@@ -1,11 +1,25 @@
+
+//*Min JS funktion *//
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
 
 }
 
 
-
+//*Min dom manipulering*//
 function changeColor(){
     document.querySelector(".javabox").style.color='darkblue';
 
 }
+
+
+
+//**JS funktion, text ändrar färg**//
+function mouseDown() {
+  document.getElementById("myP").style.color = "red";
+}
+
+function mouseUp() {
+  document.getElementById("myP").style.color = "green";
+}
+
